a8c1c77725c7fa6a389dcf59609a06ff4146e1e1
Karinaquinteros <karinaq38@gmail.com>
2025-05-16 00:16:55 -0300

    se inicia archivo de procesos.sh: se agregan opciones

--- procesos.sh
+++ procesos.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+while true; do
+	clear
+	echo "=====GESTION DE PROCESOS====="
+	echo "1. Ver procesos en ejecuciòn"
+	echo "2. Ver detalles de un proceso por PID"
+	echo "3. Enviar señal SIGTERM a un proceso"
+	echo "0. Volver al menù principal"
+	echo "============================="
+	read -p "seleccione una opción..." opcion
