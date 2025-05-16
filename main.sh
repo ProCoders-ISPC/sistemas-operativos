b4e7083a4dd7b85885ced560ef9f49994519daa2
lauzarg <lauzarate@mi.unc.edu.ar>
2025-05-16 00:06:52 -0300

    Creacion de los archivos main.sh y procesos.sh. Esta desarrollado el main y funcionando

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+	clear
+	echo "=====TOOLKIT====="
+	echo "1. Gestion de procesos"
+	echo "0. salir"
+	echo "================="
+	read -p "seleccione una opciòn:" opcion 
+
+	case $opcion in
+		1) ./procesos.sh;;
+		0) echo "saliendo del toolkit..."; exit 0;;
+		*) echo "Opciòn inválida. Presione Enter para continuar..."; read;;
+	
+	esac
+done
