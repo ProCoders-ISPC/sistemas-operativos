4f108976c5226ecf818e7e459e33f890128a414b
Daniel Paez <danicba03@gmail.com>
2025-06-06 22:57:30 -0300

    feat: se agregó la funcionalidad de memoria y disco al menu

--- main.sh
+++ main.sh
@@ -4,12 +4,16 @@ while true; do
 	clear
 	echo "=====MonitoraSys====="
 	echo "1. Gestion de procesos"
+	echo "2. Gestion de memoria"
+	echo "3. Gestion de disco"
 	echo "0. salir"
 	echo "================="
 	read -p "seleccione una opciòn:" opcion 
 
 	case $opcion in
-		1) ./procesos.sh;;
+		1) src/./procesos.sh;;
+		2) src/.memoria.sh;;
+		3) src/.disco.sh;;
 		0) echo "saliendo del toolkit..."; exit 0;;
 		*) echo "Opciòn inválida. Presione Enter para continuar..."; read;;
 	
